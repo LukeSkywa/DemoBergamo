f4293327a77eea3170afebf366815c77e3440dae
luca.alicata <luca.alicata@lynxspa.com>
2020-03-16 17:08:20 +0100

    utilizzo pipe e safenavigator

--- src/app/primo/primo.component.ts
+++ src/app/primo/primo.component.ts
@@ -14,8 +14,17 @@ export class PrimoComponent implements OnInit {
   @Input()
   titoloInput: string;
 
+  lista = [1, 2];
 
-  constructor() { }
+  myDate: Date = new Date();
+
+  oggetto: {chiave: string};
+  oggetto1: {chiave: string} = {
+    chiave:  'valore'
+  };
+
+  constructor() { 
+  }
 
   ngOnInit(): void {
   }
