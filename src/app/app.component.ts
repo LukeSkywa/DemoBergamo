87f814d482606f4e6f75d1ec0fb28d8040b79d82
luca.alicata <luca.alicata@lynxspa.com>
2020-03-16 16:34:54 +0100

    creata proprietà di input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'DemoBergamo';
 
-  htmlString = '<div><p>Prova</p></div>';
-  linkUrl = 'https://google.it';
-
+  title = 'Titolo';
 }
