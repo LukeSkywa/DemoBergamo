a436699bb2e303b12c0d8c5df6bee3e5865ec719
luca.alicata <luca.alicata@lynxspa.com>
2020-03-30 14:41:50 +0200

    utilizzo propietà di input

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,15 +1,17 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
+  @Input() highlightColor: string;
+
   constructor(private el: ElementRef) { 
   }
 
   @HostListener('mouseenter') onMouseEnter() {
-    this.el.nativeElement.style.backgroundColor = 'yellow';
+    this.el.nativeElement.style.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') onMouseLeave() {
