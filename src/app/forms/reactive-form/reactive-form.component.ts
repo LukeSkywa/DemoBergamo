57f4fcc04c066125437fb7205bffc8d624396b65
luca.alicata <luca.alicata@lynxspa.com>
2020-04-02 14:28:08 +0200

    Utilizzo Validators

--- src/app/forms/reactive-form/reactive-form.component.ts
+++ src/app/forms/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -11,10 +11,14 @@ export class ReactiveFormComponent implements OnInit {
 
   heroForm: FormGroup
 
-  constructor(private fb: FormBuilder) { 
-    this.heroForm= this.fb.group({
-      name: '',
-      power: null
+  constructor(private fb: FormBuilder) {
+    this.heroForm = this.fb.group({
+      name: ['', Validators.compose([Validators.required, Validators.minLength(2)])],
+      power: ['', Validators.required],
+      street: '',
+      city: '',
+      state: '',
+      zip: ''
     });
   }
 
