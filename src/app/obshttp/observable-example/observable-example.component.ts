67239a16e8869b2ba83fefc41e91aa5bf4adcf14
luca.alicata <luca.alicata@lynxspa.com>
2020-04-06 15:35:27 +0200

    esempi operatori obsevable

--- src/app/obshttp/observable-example/observable-example.component.ts
+++ src/app/obshttp/observable-example/observable-example.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable, of, from, interval, Subscription } from 'rxjs';
-import { filter } from 'rxjs/operators';
+import { filter, first, distinct } from 'rxjs/operators';
 
 @Component({
   selector: 'app-observable-example',
@@ -12,16 +12,50 @@ export class ObservableExampleComponent implements OnInit, OnDestroy {
   constructor() { }
 
   ngOnInit(): void {
-    this.mysubs = interval(1000).subscribe(n =>
-      console.log(`It's been ${n} seconds since subscribing!`)
-    );
+    // this.mysubs = interval(1000).subscribe(n =>
+    //   // console.log(`It's been ${n} seconds since subscribing!`)
+    // );
 
+    // esempio filter
     of(5,8,6).pipe(
       filter(value=>{
-        return true;
+        return value % 2 === 0;
       })
     ).subscribe(value=>{
-      console.log('VALUE: '+value);
+      console.log('VALUE - filter: '+value);
+    })
+
+    // esempio first
+    from(['pippo', 'pluto', 'paperino']).pipe(
+      first()
+    ).subscribe(value=>{
+      console.log('VALUE - first: '+value);
+    })
+    
+    from([5,2,7,8,6]).pipe(
+      first(value=>{
+        return value % 2 === 0;
+      })
+    ).subscribe(value=>{
+      console.log('VALUE - first: '+value);
+    })
+
+    // questo sotto è uguale a quello sopra
+    from([5,2,7,8,6]).pipe(
+      filter(value=>{
+        return value % 2 === 0;
+      }),
+      first()
+    ).subscribe(value=>{
+      console.log('VALUE - first: '+value);
+    });
+
+    // esempio distinct
+    
+    from([5,2,7, 8, 5,8, 2,6]).pipe(
+      distinct()
+    ).subscribe(value=>{
+      console.log('VALUE - distinct: '+value);
     })
   }
 
