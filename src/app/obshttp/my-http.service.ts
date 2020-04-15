f72bde8d688e33c791d5711640aa72de05b9b9e0
luca.alicata <luca.alicata@lynxspa.com>
2020-04-15 11:00:45 +0200

    setting dell'header

--- src/app/obshttp/my-http.service.ts
+++ src/app/obshttp/my-http.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpResponse, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { GameItem } from './model/game-item.interface';
 
@@ -11,7 +11,11 @@ export class MyHttpService {
   constructor(private httpClient: HttpClient) { }
 
   getGames(): Observable<HttpResponse<GameItem[]>>{
-    return this.httpClient.get<GameItem[]>('http://localhost:3000/games', { observe: 'response'});
+    let httpHeader: HttpHeaders = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+    httpHeader = httpHeader.set('Response-Type', 'application/json');
+    return this.httpClient.get<GameItem[]>('http://localhost:3000/games', { observe: 'response', headers: httpHeader});
   }
 
   getGamesErr(): Observable<any>{
