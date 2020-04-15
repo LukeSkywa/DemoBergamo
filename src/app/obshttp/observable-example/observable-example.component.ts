d917ba22c68c77cceeff9489a6a580895819dbca
luca.alicata <luca.alicata@lynxspa.com>
2020-04-15 09:11:58 +0200

    ultimi esempi con gli observable

--- src/app/obshttp/observable-example/observable-example.component.ts
+++ src/app/obshttp/observable-example/observable-example.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Observable, of, from, interval, Subscription, timer } from 'rxjs';
-import { filter, first, distinct, takeUntil } from 'rxjs/operators';
+import { Observable, of, from, interval, Subscription, timer, combineLatest } from 'rxjs';
+import { filter, first, distinct, takeUntil, takeWhile, take, map, reduce } from 'rxjs/operators';
 
 @Component({
   selector: 'app-observable-example',
@@ -64,6 +64,55 @@ export class ObservableExampleComponent implements OnInit, OnDestroy {
     ).subscribe(value=>{
       console.log('VALUE - distinct: '+value);
     });
+
+    // esempio takewhile
+    from([5,2,7, 8, 5,8, 2,6]).pipe(
+      takeWhile(value => value <8)
+    ).subscribe(value=>{
+      console.log('VALUE - takewhile: '+value);
+    });    
+
+    // esempio take
+    from([5,2,7, 8, 5,8, 2,6]).pipe(
+      take(4)
+    ).subscribe(value=>{
+      console.log('VALUE - take: '+value);
+    });
+
+    // esempio map
+    of(2,3,7,9).pipe(
+      map(value => value*value)
+    ).subscribe(value=>{
+      console.log('VALUE - map: '+value);
+    });
+    
+    of(2,3,7,9).pipe(
+      map((value, index) => {
+        switch(value){
+          case 2: return 'pippo';
+          case 7: return 'pluto';
+          default: return 'paperino';
+        }
+      })
+    ).subscribe(value=>{
+      console.log('VALUE - map: '+value);
+    });
+
+    // esempio reduce
+    of(2,3,7,9).pipe(
+      reduce((acc, value)=>{
+        return acc - value;
+      })
+    ).subscribe(value=>{
+      console.log('VALUE - reduce: '+value);
+    });
+
+    // esempio combineLatest
+    combineLatest(timer(2000, 3000), timer(1000, 4000)).subscribe(([value1, value2])=>{
+      console.log('timer1: '+value1);
+      console.log('timer2: '+value2);
+    });
+
   }
 
   ngOnDestroy(): void {
